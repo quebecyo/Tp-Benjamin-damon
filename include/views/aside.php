0328090f5b7ccaee2f76749a1205d5ec112fdc7c
quebecyo <benjamintennisandroller@hotmail.fr>
2016-05-10 08:07:59 -0400

    Tp-benjamin-damon

---
+++ include/views/aside.php
@@ -0,0 +1,12 @@
+<aside>
+    <div class="wrapper_left">
+      <h2>Type de Roman</h2>
+      <ul>
+        <li class="aside_menu"><a href="#category1" data-smoothscroll="category1">Aventure</a></li>
+        <li class="aside_menu"><a href="#category2" data-smoothscroll="category2">Science-fiction</a></li>
+        <li class="aside_menu"><a href="#category3" data-smoothscroll="category3">Biographie</a></li>
+        <li class="aside_menu"><a href="#category4" data-smoothscroll="category4">Policier</a></li>
+        <li class="aside_menu"><a href="#category5" data-smoothscroll="category5">Fantastique</a></li>
+      </ul>
+    </div>
+  </aside>
